3c4d9f622c11712fd6a8c8aa1d52484434823061
Adriano Sabanal Jr <sabanalers@gmail.com>
2023-02-25 20:30:50 +0800

    feat: DR-3

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,6 @@
 export const breakpoints = {
-  xs: '(min-width:0px) and (max-width:599px)',
+  xs: '(min-width:0px) and (max-width:599px) and (min-height:0px) and (max-height:749px)',
+  xs2: '(min-width:0px) and (max-width:599px) and (min-height:750px) and (max-height:1000px)',
   sm: '(min-width:600px) and (max-width:899px)',
   md: '(min-width:900px) and (max-width:1199px)',
   lg: '(min-width:1200px) and (max-width:1535px)',
